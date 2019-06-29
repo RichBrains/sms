ff61e573219c44ed61acd9186ae8f76df59e4ed6
Daniel <205190+snoopckuu@users.noreply.github.com>
2019-06-29 19:18:02 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
-
+cd /var/www;
+php app/console migrations:migrate --env=prod
+rm -rf app/cache/*
+php app/console cache:warmup --env=prod
+chmod 777 app/cache/ -R
 php-fpm7 -F &
 nginx -g 'daemon off;' &
-supervisord -c /var/www/app/config/supervisord.conf &
-tail -f /dev/null	
+supervisord -c /etc/supervisor/conf/supervisord.conf &
+tail -f /dev/null
